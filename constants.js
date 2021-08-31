35abc265ad4ab1876262b786bf48dfb33cfb23bc
Oscar Escobar <escobarmoragaoscar@gmail.com>
2021-08-31 17:15:15 -0400

    script automatizacion por establecimientos

--- constants.js
+++ constants.js
@@ -1,5 +1,22 @@
-module.exports = {
-    username: '4531',
-    dv: '4',
-    password: 'leon2019'
-}
+module.exports = [
+    {
+        username: '4531',
+        dv: '4',
+        password: 'leon2019'
+    },
+    {
+        username: '4540',
+        dv: '0',
+        password: 'ejercito1065'
+    },
+    {
+        username: '4543',
+        dv: '8',
+        password: '69150461'
+    },
+    {
+        username: '4544',
+        dv: '6',
+        password: '69150441'
+    } 
+]
--- prueba.js
+++ prueba.js
@@ -1,64 +1,72 @@
 const puppeteer = require('puppeteer');
 const path = require('path')
+const fs = require('fs');
 const C = require('./constants');
-
 const downloadPath = path.resolve('./download');
+
 a = 0;
+for (let index = 0; index < C.length; index++) {
+    const element = C[index];
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: true
-    });
-    const page = await browser.newPage();
-
-    await page.goto('https://sige.mineduc.cl/Sige/Login');
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
     
-    await page.click('#usuario');
-    await page.keyboard.type(C.username);
-    await page.click('#dv');
-    await page.keyboard.type(C.dv);
-    await page.click('#clave');
-    await page.keyboard.type(C.password);
-    await page.click('#ingresar-res');
+        await page.goto('https://sige.mineduc.cl/Sige/Login');
+        
+        await page.click('#usuario');
+        await page.keyboard.type(element.username);
+        await page.click('#dv');
+        await page.keyboard.type(element.dv);
+        await page.click('#clave');
+        await page.keyboard.type(element.password);
+        await page.click('#ingresar-res');
+    
+        await page.on('dialog', async (dialog) => {
+    
+            console.log(dialog.message())
+    
+            await page._client.send('Page.setDownloadBehavior', {
+                behavior: 'allow',
+                downloadPath: path.resolve('./download/' + element.username)
+            })
+    
+            if (a == 0) {
+                await dialog.dismiss()
+                await page.click('#ingresar-res')
+                await page.waitForTimeout(2000)
+                
+                //await page.screenshot({ path: 'screenshot.png', fullPage: true })
+                await page.evaluate(() => (enviar("","../Reportes/ExportarNomina")))
+    
+                await page.waitForTimeout(2000)
+    
+                await page.select('#cmbAno', '2021')
+                a = 1;
+    
+                await page.waitForTimeout(2000)
+                //await page.screenshot({ path: 'nomina.png', fullPage: true })
+                
+                await page.evaluate(() => (exportaNomina(3)))
 
-    await page.on('dialog', async (dialog) => {
+                await fs.rename('./download/nomina_excel.xls', './download/'+ element.username + '.xls');
+    
+                await page.waitForTimeout(2000)
+    
+            } else {
+    
+                await dialog.accept()
+    
+                await page.waitForTimeout(2000)
 
-        console.log(dialog.message())
+            }
+    
+            await browser.close();
+        });
+    })();
 
-        await page._client.send('Page.setDownloadBehavior', {
-            behavior: 'allow',
-            downloadPath: downloadPath 
-        })
 
-        if (a == 0) {
-            await dialog.dismiss()
-            await page.click('#ingresar-res')
-            await page.waitForTimeout(2000)
-            
-            //await page.screenshot({ path: 'screenshot.png', fullPage: true })
-            await page.evaluate(() => (enviar("","../Reportes/ExportarNomina")))
-
-            await page.waitForTimeout(2000)
-
-            await page.select('#cmbAno', '2021')
-            a = 1;
-
-            await page.waitForTimeout(2000)
-            //await page.screenshot({ path: 'nomina.png', fullPage: true })
-            
-            await page.evaluate(() => (exportaNomina(3)))
-
-            await page.waitForTimeout(2000)
-
-        } else {
-
-            await dialog.accept()
-
-            console.log('Si')
-
-            await page.waitForTimeout(2000)
-        }
-
-        await browser.close();
-    });
-})();
+}
+    
